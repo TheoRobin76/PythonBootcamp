f768f85d1e46188a282728118d812f9f1c802111
Your Name <you@example.com>
2021-08-09 13:03:02 +0100

    Day 6 - Robot Maze

---
+++ Day6_robot_maze.py
@@ -0,0 +1,45 @@
+# Robot Hurdles
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+
+# Robot Maze
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
