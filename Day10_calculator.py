69882dbbb273239f44e2eb4a4a808be5474cbf2f
Your Name <you@example.com>
2021-08-12 17:00:08 +0100

    Day 10 - Calculator

---
+++ Day10_calculator.py
@@ -0,0 +1,60 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+calculator = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+
+def calculate():
+    print(logo)
+    num1 = float(input("Please enter the first number: "))
+    for key in calculator:
+        print(key)
+
+    should_continue = True
+    while should_continue:
+        operation = input("Please choose an operation to perform: ")
+        num2 = float(input("Please enter the second number: "))
+        function = calculator[operation]
+        result1 = function(num1, num2)
+        print(f"{num1} {operation} {num2} = {result1}")
+
+        again = input("Would you like to perform another operation? (y/n): ").lower()
+        if again == "y":
+            num1 = result1
+        else:
+            should_continue = False
+            calculate()
+
+
+calculate()
