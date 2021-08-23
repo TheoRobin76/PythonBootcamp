653064513558687de502cb2bd5ceb4084be77c08
Your Name <you@example.com>
2021-08-23 11:10:44 +0100

    Day 16 - OOP Coffee Machine

---
+++ OOP_turtle.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+# turtle documentation: https://docs.python.org/3/library/turtle.html
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# PyPi: https://pypi.org/
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table.align)
+print(table)
+
+
