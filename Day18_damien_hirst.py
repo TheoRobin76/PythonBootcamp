25c537c349fdc29aed2a982036c317d0d60def3d
Your Name <you@example.com>
2021-08-26 10:45:25 +0100

    Day 19 - Turtle Race

--- Day18_damien_hirst.py
+++ Day18_damien_hirst.py
@@ -7,78 +7,78 @@ import colorgram
 # turtle colours: https://cs111.wellesley.edu/labs/lab01/colors
 # more turtle colours: https://trinket.io/docs/colors
 
-tim = Turtle()
-tim.shape("turtle")
-tim.color("DarkSlateBlue")
-
-# draw a square
-for i in range(4):
-    tim.forward(100)
-    tim.right(90)
-
-# draw a dashed line
-for i in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
-
-# drawing different shapes
-colours = ["indigo", "DarkSlateBlue", "OrangeRed", "LightSeaGreen",
-           "CornFlowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
-           "SlateGray", "wheat", "orange", "black", "ForestGreen", "green3"]
-
-
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for i in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-
-for shape in range(3, 11):
-    tim.color(random.choice(colours))
-    draw_shape(shape)
-
-
-# random walk
-t.colormode(255)
-angles = [0, 90, 180, 270]
-tim.pensize(10)
-tim.speed("fastest")
-
-
-def random_colour():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    colour = (r, g, b)
-    return colour
-
-
-for i in range(500):
-    tim.forward(30)
-    tim.setheading(random.choice(angles))
-    tim.color(random_colour())
-
-
-# draw a spirograph
-for i in range(72):
-    tim.color(random_colour())
-    tim.circle(100)
-    tim.left(5)
-
-# Damien Hirst spot painting
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
-
-print(rgb_colors)
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("DarkSlateBlue")
+#
+# # draw a square
+# for i in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+#
+# # draw a dashed line
+# for i in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+#
+# # drawing different shapes
+# colours = ["indigo", "DarkSlateBlue", "OrangeRed", "LightSeaGreen",
+#            "CornFlowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
+#            "SlateGray", "wheat", "orange", "black", "ForestGreen", "green3"]
+#
+#
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for i in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+#
+# for shape in range(3, 11):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape)
+#
+#
+# # random walk
+# t.colormode(255)
+# angles = [0, 90, 180, 270]
+# tim.pensize(10)
+# tim.speed("fastest")
+#
+#
+# def random_colour():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     colour = (r, g, b)
+#     return colour
+#
+#
+# for i in range(500):
+#     tim.forward(30)
+#     tim.setheading(random.choice(angles))
+#     tim.color(random_colour())
+#
+#
+# # draw a spirograph
+# for i in range(72):
+#     tim.color(random_colour())
+#     tim.circle(100)
+#     tim.left(5)
+#
+# # Damien Hirst spot painting
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
 color_list = [(201, 164, 112), (152, 75, 50), (221, 201, 138), (57, 95, 126),
               (170, 152, 44), (138, 31, 20), (135, 163, 183), (196, 94, 75),
---
+++ Day19_turtle_games.py
@@ -0,0 +1,74 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+screen1 = Screen()
+
+
+# Etch a Sketch
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def counter_clockwise():
+    tim.left(10)
+
+
+def clockwise():
+    tim.right(10)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen1.listen()
+screen1.onkeypress(key="w", fun=move_forwards)
+screen1.onkeypress(key="s", fun=move_backwards)
+screen1.onkeypress(key="a", fun=counter_clockwise)
+screen1.onkeypress(key="d", fun=clockwise)
+screen1.onkeypress(key="c", fun=clear)
+screen1.exitonclick()
+
+
+# Turtle Race
+is_race_on = False
+screen2 = Screen()
+screen2.setup(width=500, height=400)
+user_bet = screen2.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = []
+
+y = -100
+for i in colors:
+    new_turtle = Turtle("turtle")
+    new_turtle.color(i)
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y)
+    all_turtles.append(new_turtle)
+    y += 40
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 240:
+            winning_color = turtle.pencolor()
+            is_race_on = False
+            screen2.clear()
+            if winning_color == user_bet:
+                print(f"You have won, the {winning_color} turtle was victorious!")
+            else:
+                print(f"You have lost, the {winning_color} turtle was victorious!")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
