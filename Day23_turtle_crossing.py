eb7e5a00fe6032d5c16781b856e5b2c498f9a293
Your Name <you@example.com>
2021-08-31 16:44:42 +0100

    Day 23 - Turtle Crossing

---
+++ Day23_turtle_crossing.py
@@ -0,0 +1,39 @@
+import time
+from turtle import Screen
+from turtle_crossing import CarManager, Player, Scoreboard, FINISH_LINE_Y
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+# instantiate the classes
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+# keyboard input for the turtle
+screen.listen()
+screen.onkeypress(player.up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    # create and move the cars
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # detect collision with a car
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    # detect when turtle reaches the next level
+    if player.ycor() > FINISH_LINE_Y:
+        player.next_level()
+        car_manager.level_up()
+        scoreboard.new_level()
+
+screen.exitonclick()
---
+++ turtle_crossing.py
@@ -0,0 +1,78 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.penup()
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.goto(310, random.randint(-250, 250))
+            new_car.color(random.choice(COLORS))
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.next_level()
+        self.setheading(90)
+
+    def up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def next_level(self):
+        self.goto(STARTING_POSITION)
+
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-200, 260)
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def new_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
+
+
