919499b37912982ef935c3bb932c315686907168
Your Name <you@example.com>
2021-09-30 14:01:11 +0100

    Day 32 - Birthday Emails

--- Day32_birthday_emails/motivational_emails.py
+++ Day32_birthday_emails/motivational_emails.py
@@ -2,31 +2,30 @@ import smtplib
 import random
 import datetime as dt
 
-yahoomail = "myleslangston@hotmail.com"
+myles = ""
+theo = ""
+antonia = ""
 gmail = "seanbean156@gmail.com"
-password = "3hlT+P7u&Mr*z!A"
+password = ""
 
 # finding the day of the week
 now = dt.datetime.now()
 year = now.year
-month = now.month
+hour = now.hour
 day_of_week = now.weekday()
-print(day_of_week)
 
-if day_of_week == 3:
-    # choosing a random motivational quote
-    with open("quotes.txt", 'r') as quotes:
-        all_quotes = quotes.readlines()
-        random_quote = random.choice(all_quotes)
-    print(random_quote)
+# choosing a random motivational quote
+with open("quotes.txt", 'r') as quotes:
+    all_quotes = quotes.readlines()
+    random_quote = random.choice(all_quotes)
+print(random_quote)
 
-    # emailing the quote
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=gmail, password=password)
-        connection.sendmail(
-            from_addr=gmail,
-            to_addrs=yahoomail,
-            msg=f"Subject:Motivation\n\n{random_quote}"
-        )
-
+# emailing the quote
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=gmail, password=password)
+    connection.sendmail(
+        from_addr=gmail,
+        to_addrs=(myles, theo, antonia),
+        msg=f"Subject:Motivation\n\n{random_quote}"
+    )
